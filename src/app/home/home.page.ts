7886ba52fcd49b5b75ff02c58558b354658909b7
juliagomes2007 <juliagomes2007@gmail>
2024-02-15 08:18:44 -0300

    teste

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
   constructor() {}
 
+  n1 = '';
+  n2 = '';
+  res = 0;
+
+  somar(){
+    this.res = parseFloat(this.n1) + parseFloat(this.n2);
+  }
 }
